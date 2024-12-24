69a8cc6616807361f1d7890924623e593431bb55
monoira <monoira@users.noreply.github.com>
2024-12-24 10:44:15 +0400

    feat: added .editorconfig to all projects and improved documentation in percento

--- 2024-10-27_percento/vite.config.ts
+++ 2024-10-27_percento/vite.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
-// || automatic image compression upon build ( Must install libraries named sharp and svgo as -D )
+// || automatic image compression upon build
+// WARN: Must install libraries named sharp and svgo as -D
+// npm i -D sharp svgo
 import Sitemap from "vite-plugin-sitemap";
 import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
 
