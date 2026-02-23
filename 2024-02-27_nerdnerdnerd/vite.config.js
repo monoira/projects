b38b91857fd942597ec9dabd69a03650ae8b1c04
monoira <monoira@users.noreply.github.com>
2026-02-23 21:27:31 +0400

    feat: removed outdated project

--- 2024-02-27_nerdnerdnerd/vite.config.js
+++
@@ -1,8 +0,0 @@
-import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
-
-export default {
-  plugins: [
-    // image compression. happens when using "npm run build"
-    ViteImageOptimizer(),
-  ],
-};
