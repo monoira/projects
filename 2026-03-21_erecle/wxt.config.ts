386b0ca595d5cb49ef61d3d37b4c12e8421afe85
monoira <monoira@users.noreply.github.com>
2026-03-22 16:38:43 +0400

    feat: improved package description

--- 2026-03-21_erecle/wxt.config.ts
+++ 2026-03-21_erecle/wxt.config.ts
@@ -1,6 +1,16 @@
-import { defineConfig } from 'wxt';
+import { defineConfig } from "wxt";
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
-  modules: ['@wxt-dev/module-react'],
+  modules: ["@wxt-dev/module-react"],
+  manifest: {
+    browser_specific_settings: {
+      gecko: {
+        id: "wxt-erecle@gmail.com",
+        data_collection_permissions: {
+          required: ["none"],
+        },
+      },
+    },
+  },
 });
